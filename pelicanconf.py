76612a050e7053ae8d6c078d3db4c9591a93e9c3
aki-mia <13302223+aki-mia@users.noreply.github.com>
2024-05-12 10:13:52 +0900

    Add .gitignore, Makefile, and content files
    .gitignore and a comprehensive Makefile were added for project management. Several new content files related to privacy policy for Chrome extensions and other information were also added.

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+AUTHOR = 'aki-mia'
+SITENAME = '個人開発とかのブログ'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = 'ja'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+)
+
+# Social widget
+SOCIAL = (
+    ("GitHub", "https://github.com/aki-mia"),
+    ("Twitter", "https://twitter.com/aki_honmono"),
+    ("LinkedIn", "https://www.linkedin.com/in/akihirotanii/"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
